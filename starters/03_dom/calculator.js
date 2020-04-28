740a73e4e56e4d46b546a1bbd78f65b7c3ece3c7
gespina <glenn.espina@gmail.com>
2020-04-28 13:50:22 -0700

    Solution for calculator problem

--- starters/03_dom/calculator.js
+++ starters/03_dom/calculator.js
@@ -1,1 +1,33 @@
-// Put your calculator's JavaScript here.
+let input = document.querySelector('input');
+let inputDisplay = 0;
+let firstNumber = 0;
+let operator = '';
+const readButton = (btn) => {
+  let btnTxt = btn.srcElement.textContent;
+  if (/[0-9.]/.test(btnTxt)) {
+    input.value = input.value + btnTxt;
+  } else if (/[\+\-\x\/]/.test(btnTxt)) {
+    firstNumber = input.value;
+    input.value = '';
+    operator = btnTxt;
+  } else if (btnTxt === '=') {
+    switch (operator) {
+      case '+':
+        input.value = +firstNumber + +input.value;
+        break;
+      case '-':
+        input.value = +firstNumber - +input.value;
+        break;
+      case '*':
+        input.value = +firstNumber * +input.value;
+        break;
+      case '/':
+        input.value = +firstNumber / +input.value;
+        break;
+    }
+  }
+};
+
+let allButtons = [...document.querySelectorAll('button')].map((btn) =>
+  btn.addEventListener('click', readButton)
+);
